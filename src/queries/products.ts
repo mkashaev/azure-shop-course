73775fa2f3a1b2ab64efe7a78f973d41f574670a
Marat Kashaev <marat.kashaev@pearson.com>
2025-05-02 00:26:24 +0300

    add functions

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -5,15 +5,10 @@ import { useQuery, useQueryClient, useMutation } from "react-query";
 import React from "react";
 
 export function useAvailableProducts() {
-  return useQuery<AvailableProduct[], AxiosError>(
-    "available-products",
-    async () => {
-      const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
-      );
-      return res.data;
-    }
-  );
+  return useQuery<AvailableProduct[], AxiosError>("available-products", async () => {
+    const res = await axios.get<AvailableProduct[]>(`${API_PATHS.bff}/product`);
+    return res.data;
+  });
 }
 
 export function useInvalidateAvailableProducts() {
@@ -28,9 +23,7 @@ export function useAvailableProduct(id?: string) {
   return useQuery<AvailableProduct, AxiosError>(
     ["product", { id }],
     async () => {
-      const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.bff}/product/${id}`
-      );
+      const res = await axios.get<AvailableProduct>(`${API_PATHS.bff}/product/${id}`);
       return res.data;
     },
     { enabled: !!id }
@@ -40,8 +33,7 @@ export function useAvailableProduct(id?: string) {
 export function useRemoveProductCache() {
   const queryClient = useQueryClient();
   return React.useCallback(
-    (id?: string) =>
-      queryClient.removeQueries(["product", { id }], { exact: true }),
+    (id?: string) => queryClient.removeQueries(["product", { id }], { exact: true }),
     []
   );
 }
